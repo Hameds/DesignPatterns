b67d9580a1870eea1dc64ee6a8f7719e63922082
Hamed Saeedifard <hameds@gmail.com>
2018-04-28 00:09:43 +0430

    add prototype call example

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using DesignPatterns.Creational.FactoryMethod;
+using DesignPatterns.Creational.Prototype;
 
 namespace DesignPatterns
 {
@@ -9,8 +10,10 @@ namespace DesignPatterns
         {
             Console.WriteLine("Welcome to design patterns sample. watch design pattern videos (in Persian): http://youtube.com/softwaretalks");
 
-            //Call factory method example
+            //Call Creational Design Patterns examples
+            Console.WriteLine("calling creational design patterns examples");
             FactoryMethodExample();
+            PrototypeExample();
 
 
             Console.WriteLine("press any key to exit ...");
@@ -19,6 +22,7 @@ namespace DesignPatterns
 
 
         #region Call Creational Design Patterns
+
         public static void FactoryMethodExample()
         {
             Console.WriteLine("Factory Method example");
@@ -28,6 +32,24 @@ namespace DesignPatterns
             var ghazizadehHashemi = new HealthMinister();
             ghazizadehHashemi.Manage();
         }
+
+        public static void PrototypeExample()
+        {
+            Console.WriteLine("Prototype example");
+
+            var branch1 = new BankBranch("1", "Behnam");
+            Console.WriteLine(branch1.Code);
+            Console.WriteLine(branch1.ManagerName);
+
+            var branch2 = branch1.Clone();
+            branch2.Code = "2";
+            branch2.ManagerName = "Maryam";
+            Console.WriteLine(branch2.Code);
+            Console.WriteLine(branch2.ManagerName);
+            Console.WriteLine(branch1.Code);
+            Console.WriteLine(branch1.ManagerName);
+        }
+
         #endregion
     }
 }
